eda88811aae8c374e93df29fdd4c92a8af7f9068
tianke.lyq <tianke.lyq@alibaba-inc.com>
2018-08-28 10:32:16 +0800

    move tools to lib file

--- absoluteSort.js
+++ absoluteSort.js
@@ -1,27 +1,32 @@
 // 核心代码
 const absoluteSort = (list1) => {
-  let ans = []
-  const collectRow = (currentList) => {
-      if (currentList.length == 0) {
-          return;
-      }
-      // 寻找最小 y
-      const minYElement = findMinBy(currentList, 'y');
-      // 通过最小 y 定位最小行的元素
-      const minRowElements = currentList.filter(item => item.y - minYElement.y <= 50);
-      // 当前行按照 x 升序排列
-      minRowElements.sort((a, b) => {
-          if (a.x < b.x) {
-              return -1;
-          } else if (a.x > b.x) {
-              return 1;
-          }
-          return 0;
-      })
-      ans = ans.concat(minRowElements);
-      const remainList = currentList.filter(item => item.y - minYElement.y > 50);
-      collectRow(remainList)
-  };
-  collectRow(list1);
-  return ans
+    let ans = []
+    const findMinBy = (arr, key) => {
+        return arr.reduce(function (prev, curr, index, arr) {
+            return prev[key] < curr[key] ? prev : curr;
+        });
+    }
+    const collectRow = (currentList) => {
+        if (currentList.length == 0) {
+            return;
+        }
+        // 寻找最小 y
+        const minYElement = findMinBy(currentList, 'y');
+        // 通过最小 y 定位最小行的元素
+        const minRowElements = currentList.filter(item => item.y - minYElement.y <= 50);
+        // 当前行按照 x 升序排列
+        minRowElements.sort((a, b) => {
+            if (a.x < b.x) {
+                return -1;
+            } else if (a.x > b.x) {
+                return 1;
+            }
+            return 0;
+        })
+        ans = ans.concat(minRowElements);
+        const remainList = currentList.filter(item => item.y - minYElement.y > 50);
+        collectRow(remainList)
+    };
+    collectRow(list1);
+    return ans
 }
